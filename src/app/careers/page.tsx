d4a7f2a16d784d9efe1a785da7093abab4084279
Fábio Cunha <fabiorafaelcoutada@disquiet-colab.com>
2025-10-21 13:16:27 +0100

    Remove unused Careers, Partners, and Privacy Policy pages

--- src/app/careers/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Careers() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">Careers</h1>
-    </main>
-  );
-}
