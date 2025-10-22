ff064f8db63917a4bb3da41728bc2846cbdb184f
Fábio Cunha <fabiorafaelcoutada@disquiet-colab.com>
2025-10-22 19:33:11 +0100

    Refactor Tailwind CSS config: use ES module syntax and optimize typography plugin setup.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
+import typography from '@tailwindcss/typography';
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const config = {
     content: [
         "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,7 +10,7 @@ module.exports = {
     theme: {
         extend: {},
     },
-    plugins: [
-        require('@tailwindcss/typography'), // <-- Add this line
-    ],
-}
+    plugins: [typography],
+};
+
+export default config;
