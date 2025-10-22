5c3a4bf4b66bc6f2e68e69680cb0a7d7c4b79f7b
Fábio Cunha <fabiorafaelcoutada@disquiet-colab.com>
2025-10-22 20:14:14 +0100

    Wrap `<children>` in `<main>` element for semantic structure improvement.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Header />
-        {children}
+            <main>
+                {children}
+            </main>
         <Footer />
       </body>
     </html>
