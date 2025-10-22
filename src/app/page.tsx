b8683c1cbd38763c4c0557e834cee1881d21f878
Fábio Cunha <fabiorafaelcoutada@disquiet-colab.com>
2025-10-22 20:14:06 +0100

    Refactor layout: replace `<main>` with `<div>` for flexible growth and improved alignment.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,12 @@ export default async function Home() {
 
     // 2. Render it
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div className="flex flex-col items-center p-24 flex-grow">
             <div className="max-w-4xl w-full text-lg space-y-8">
                 <h1 className="text-4xl font-bold mb-4">{data.title}</h1>
                 <h2 className="text-2xl text-gray-600 mb-12">{data.subtitle}</h2>
                 <div className="prose prose-lg" dangerouslySetInnerHTML={{ __html: contentHtml }} />
             </div>
-        </main>
+        </div>
     );
 }
