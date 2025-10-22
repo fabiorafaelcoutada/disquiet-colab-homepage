9dc43b7ced14d56da8e17d2d74e71de3c72d0098
Fábio Cunha <fabiorafaelcoutada@disquiet-colab.com>
2025-10-22 20:14:38 +0100

    Refactor contact layout: replace `<main>` with `<div>` and add `flex-grow` for improved flexibility and alignment.

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,7 @@
 export default function Contact() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">Contact</h1>
-    </main>
+      <div className="flex flex-col items-center p-24 flex-grow">
+        <h1 className="text-4xl font-bold">Contact</h1>
+      </div>
   );
 }
