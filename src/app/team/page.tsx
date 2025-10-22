cc4157c3fe9eff16e625967f0e5ceefb3617cf5a
Fábio Cunha <fabiorafaelcoutada@disquiet-colab.com>
2025-10-22 20:15:05 +0100

    Refactor team layout: replace `<main>` with `<div>` and add `flex-grow` for improved alignment and responsiveness.

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -1,7 +1,7 @@
 export default function Team() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="max-w-4xl mx-auto p-4 md:p-8 flex-grow">
       <h1 className="text-4xl font-bold">Team</h1>
-    </main>
+    </div>
   );
 }
