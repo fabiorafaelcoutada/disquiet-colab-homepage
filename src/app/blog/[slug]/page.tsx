77bfef302cde996e5f1e2c39ddc8d15a1cdbeb6e
Fábio Cunha <fabiorafaelcoutada@disquiet-colab.com>
2025-10-22 20:14:30 +0100

    Refactor blog layout: replace `<main>` with `<div>` and add `flex-grow` for improved flexibility and alignment.

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -9,7 +9,7 @@ export default async function BlogPost({ params }: Props) {
     const { data, contentHtml } = await getMarkdownContent(`blog/${params.slug}`);
 
     return (
-        <main className="flex min-h-screen flex-col items-center p-4 md:p-24">
+        <div className="flex flex-col items-center p-24 flex-grow">
             <div className="max-w-4xl w-full text-lg space-y-8">
                 {/* Post Header */}
                 <h1 className="text-4xl font-bold mb-2">{data.title}</h1>
@@ -21,6 +21,6 @@ export default async function BlogPost({ params }: Props) {
                     dangerouslySetInnerHTML={{ __html: contentHtml }}
                 />
             </div>
-        </main>
+        </div>
     );
 }
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -5,7 +5,7 @@ export default function BlogIndex() {
     const allPosts = getAllPosts();
 
     return (
-        <div className="max-w-4xl mx-auto p-4 md:p-8">
+        <div className="max-w-4xl mx-auto p-4 md:p-8 flex-grow">
             <h1 className="text-4xl font-bold mb-8">Blog</h1>
 
             <div className="space-y-6">
