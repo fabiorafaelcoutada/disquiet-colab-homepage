737c083d17deb76fc2eefa3b7217d2d4e7147249
Fábio Cunha <fabiorafaelcoutada@disquiet-colab.com>
2025-10-25 17:36:31 +0100

    Update `page.tsx`: replace `flex-grow` with `grow` to align with Tailwind CSS v4 syntax

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,7 +2,7 @@ import styles from './page.module.css'; // 1. Import our new CSS Module
 
 export default function Contact() {
   return (
-      <div className="flex flex-col items-center p-24 flex-grow">
+      <div className="flex flex-col items-center p-24 grow">
         <h1 className="text-4xl font-bold">Contact</h1>
       </div>
   );
