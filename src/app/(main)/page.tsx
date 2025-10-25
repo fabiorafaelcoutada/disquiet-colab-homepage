c9c970b4eb30c15f92d778e01cfb257e28c51dfc
Fábio Cunha <fabiorafaelcoutada@disquiet-colab.com>
2025-10-25 17:36:18 +0100

    Update `page.tsx`: replace `flex-grow` with `grow` to align with Tailwind CSS v4 syntax.

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
 
     // 2. Render it
     return (
-        <div className="flex flex-col items-center p-24 flex-grow">
+        <div className="flex flex-col items-center p-24 grow">
             <div className="max-w-4xl w-full text-lg space-y-8">
                 <h1 className="text-4xl font-bold mb-4">{data.title}</h1>
                 <h2 className="text-2xl text-gray-600 mb-12">{data.subtitle}</h2>
